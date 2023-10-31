c4c941bfec80e9540a88ea100e1bb670d5068c93
maberizk <michelle.aberizk@gmail.com>
2023-10-31 15:22:54 -0700

    added light/dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import "./index.css";
 
 function App() {
+  function switchTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    switchTheme("dark");
+  }
+
   return (
     <>
-      <body>
-        devfinder Light Dark Search GitHub username... Search Joined Repos
-        Followers Following
-      </body>
+      <div
+        className="app"
+        style={{
+          backgroundColor: "var(--color-background)",
+          color: "var(--color-text)",
+        }}
+      >
+        <div className="heading">
+          <h1>devFinder</h1>
+          <button onClick={() => switchTheme("light")}>Light</button>
+          <button onClick={() => switchTheme("dark")}>Dark</button>
+        </div>
+        <div className="searchBar">
+          <input type="text" placeholder="Search Github username..."></input>
+          <button>Search</button>
+        </div>
+        <div className="profile">
+          <img src=""></img>
+        </div>
+      </div>
     </>
   );
 }
