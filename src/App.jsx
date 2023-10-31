6b4b2c689aa29a34627cd04c4de921795fc46d72
maberizk <michelle.aberizk@gmail.com>
2023-10-31 14:13:53 -0700

    organzing file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <body>
+        devfinder Light Dark Search GitHub username... Search Joined Repos
+        Followers Following
+      </body>
+    </>
+  );
+}
+
+export default App;
