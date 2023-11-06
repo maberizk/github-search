3b198bad4b7fd92d278e0cf9a4ece3fb1936396f
maberizk <michelle.aberizk@gmail.com>
2023-11-06 10:58:58 -0800

    working on responsiveness

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -31,21 +31,21 @@ const Profile = ({ username }) => {
         color: "var(--color-text)",
       }}
     >
+      {/* <div className="userDetailsAll"> */}
       <div className="userDetails">
         <img src={user.avatar_url} alt="User avatar" className="userImg" />
-        <div className="detailsWBio">
-          <div className="detailswithDate">
-            <div className="mainDetails">
-              <h1 className="userName">{user.name}</h1>
-              <p className="login">@{user.login}</p>
-            </div>
-            <p>{formattedDate}</p>
+        {/* <div className="detailsWBio"> */}
+        <div className="detailswithDate">
+          <div className="mainDetails">
+            <h1 className="userName">{user.name}</h1>
+            <p className="login">@{user.login}</p>
           </div>
-          <p className="bio">
-            {user.bio ? user.bio : "This profile has no bio"}
-          </p>
+          <p>{formattedDate}</p>
         </div>
+        {/* </div> */}
       </div>
+      <p className="bio">{user.bio ? user.bio : "This profile has no bio"}</p>
+      {/* </div> */}
       <div
         className="githubDetails"
         style={{
